230e238a9b2b51cf45e476149d48fdc8f17e4497
sultanchik1serik <142357734+sultanchik1serik@users.noreply.github.com>
2023-12-15 22:44:49 -0500

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,26 @@
+        for (int item : data) {
+    }
+        int[] data = new int[10];
+}
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+
+        }
+        }
+
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {    main()
+
+
+def generate_random_data():
+    data = generate_random_data()
+if __name__ == "__main__":
+    return data
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
+        print(f"Random Number: {item}")
+
+def main():
+import random
